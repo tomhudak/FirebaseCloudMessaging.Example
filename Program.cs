805164ae438f60e288fd866a13baaf25349ccded
ther89 <rebel89@gmail.com>
2020-03-21 22:37:46 +0100

    Code added

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FirebaseCloudMessaging.Example
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var defaultApp = FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "token.json")),
+            });
+            Console.WriteLine(defaultApp.Name); // "[DEFAULT]"
+
+            var messaging = FirebaseMessaging.DefaultInstance;
+
+            var message = new Message()
+            {
+                Data = new Dictionary<string, string>()
+                {
+                   ["FirstName"] = "John",
+                   ["LastName"] = "Doe"
+                },
+                Notification = new Notification
+                {
+                    Title = "Message Title",
+                    Body = "Message Body"
+                },
+
+                //Token = "d3aLewjvTNw:APA91bE94LuGCqCSInwVaPuL1RoqWokeSLtwauyK-r0EmkPNeZmGavSG6ZgYQ4GRjp0NgOI1p-OAKORiNPHZe2IQWz5v1c3mwRE5s5WTv6_Pbhh58rY0yGEMQdDNEtPPZ_kJmqN5CaIc",
+                Topic = "news"
+            };
+
+            var result = await messaging.SendAsync(message);
+            Console.WriteLine(result); //projects/myapp/messages/2492588335721724324
+        }
+    }
+}
